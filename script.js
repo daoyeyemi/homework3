94ffc38c86ceed153bb519335b47d66787bc570e
daogen <daoyeyemi@aol.com>
2020-07-28 00:30:03 -0700

    added prompt questions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Prompt for length of password (btwn 8 and 128 characters)
+prompt("What length would you prefer your password to be? (Must be at least 8 characters)")
 // Prompt for lowercase, uppercase, numeric, and special characters 
+prompt("Would you like lowercase letters to be included?")
+prompt("Would you like uppercase letters to be included?")
+prompt("Would you like numbers to be included? (0-9) ")
+prompt("Would you like special characters to be included?")
 // Push array possibly?
 // Input should be validated
 // Prompts answered ---> password is generated
