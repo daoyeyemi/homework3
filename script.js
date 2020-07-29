ecc2bdfa85487e56c8e3a1c61f70744f7038906d
daogen <daoyeyemi@aol.com>
2020-07-28 18:59:17 -0700

    created a few variables and arrays

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+// Create possible arrays for upper, lower, special, numbers
+var lowerArr = "abcdefghijklmnopqrstuvwxyz"
+var upperArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numberArr = "0123456789"
+var specialArr = "!@#$%&*?/"
 // Prompt for length of password (btwn 8 and 128 characters)
-prompt("What length would you prefer your password to be? (Must be at least 8 characters)")
+var pwdLength = prompt("What length would you prefer your password to be? (Must be at least 8 characters)")
+  if (pwdLength < 8) {
+    prompt("Password length must be at least 8 characters")
+  } else(Math.floor(Math.random))
 // create array
 // Prompt for lowercase, uppercase, numeric, and special characters 
-prompt("Would you like lowercase letters to be included?")
-// create lowercase array
-prompt("Would you like uppercase letters to be included?")
-// create uppercase array
-prompt("Would you like numbers to be included? (0-9) ")
-// create number array
-prompt("Would you like special characters to be included?")
-//create special character array
+
+// create variable for lowercase letter confirmation
+var possibleLower = confirm("Would you like lowercase letters to be included?")
+  if (possibleLower===true) {
+    // push array
+  }
+  // create variable for uppercase letter confirmation
+  var possibleUpper = confirm("Would you like uppercase letters to be included?")
+  if (ossibleUpper===true) {
+    // push array
+  }
+// create variable for number confirmation
+var possibleNumbers = confirm("Would you like numbers to be included? (0-9) ")
+
+// create variable for special character confirmation
+var possibleSpecial = confirm("Would you like special characters to be included?")
+
 // Push array possibly?
 // Input should be validated
+confirm("We have considered your criteria. Press the 'Generate Password' button to recieve your password.")
 // Prompts answered ---> password is generated
 // Create loops for password generation
 // After password is generated, password is displayed on page or in alert
